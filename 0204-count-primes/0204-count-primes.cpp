244b9901e17331216e3c5b5d6c6829a2789ecd87
Kaustav Paul <paulkaustav0@gmail.com>
2026-09-01 17:26:03 +0530

    Time: 541 ms (15.44%), Space: 40.4 MB (23.16%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int countPrimes(int n) {
         if (n <= 2) return 0;
-        
-        // Only track odd numbers to cut operations in half
         vector<bool> isprime(n, true);
-        int ans = n / 2; // Assume all odd numbers >= 3 are prime initially (n/2 items)
+        int ans = n / 2; 
 
         for (int i = 3; (long long)i * i < n; i += 2) {
             if (isprime[i]) {
                 for (long long j = (long long)i * i; j < n; j += 2 * i) {
                     if (isprime[j]) {
                         isprime[j] = false;
-                        ans--; // Decrement count when marking an odd composite
+                        ans--; 
                     }
                 }
             }
